51f19910028278b06f98fcd44148d63fc6616b98
bin matsui <BinMatsui@hotmail.com>
2023-09-07 23:33:26 +0900

    Refactor

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,6 @@
-import {factorial, fizzBuzz} from './usagecases';
+import {fizzBuzz} from './usagecases';
 
 const main = () => {
-  console.log('----factorial');
-  for (let i = 1; i <=15; i++) {
-    console.log(`    factorial(${i}) = ${factorial(i)}`);
-  }
-
   console.log('----FizzBuzz');
   for (let i = 0; i <=16; i++) {
     console.log(`    FizzBuzz(${i}) = ${fizzBuzz(i)}`);
