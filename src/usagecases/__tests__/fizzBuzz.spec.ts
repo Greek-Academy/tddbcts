e9234d1fb364869139f5237f5d5cf29232cc7769
bin matsui <BinMatsui@hotmail.com>
2023-09-07 23:46:44 +0900

    3 & 5

--- src/usagecases/__tests__/fizzBuzz.spec.ts
+++ src/usagecases/__tests__/fizzBuzz.spec.ts
@@ -3,4 +3,13 @@ describe('fizzBuzz', () => {
   it('3の時にFizzを返す', () => {
     expect(fizzBuzz(3)).toBe('Fizz');
   });
+  it('5の時にBuzzを返す', () => {
+    expect(fizzBuzz(5)).toBe('Buzz');
+  });
+  it('6の時にFizzを返す', () => {
+    expect(fizzBuzz(6)).toBe('Fizz');
+  });
+  it('10の時にBuzzを返す', () => {
+    expect(fizzBuzz(10)).toBe('Buzz');
+  });
 })
--- src/usagecases/fizzBuzz.ts
+++ src/usagecases/fizzBuzz.ts
@@ -1,4 +1,6 @@
 export const fizzBuzz = (n: number): string => {
   // TODO: FizzBuzz問題をクリアするためのコードを書く
-  return 'Fizz';
+  if(n % 3 == 0) return "Fizz";
+  else if(n % 5 == 0) return "Buzz";
+  return '';
 }
