9a654f652cafa1c0ec7347ef6fac03768d66cbae
阪本良介 <sakamoto@sumire-sakamoto.co.jp>
2023-09-09 16:01:19 +0900

    fix

--- src/usagecases/__tests__/vendingMachine.spec.ts
+++ src/usagecases/__tests__/vendingMachine.spec.ts
@@ -1,8 +1,8 @@
-import { VendingMachine } from '../vendingMachine';
+import {VendingMachine} from '../vendingMachine';
 describe('vendingMachine', () => {
   it('10円を投入して合計値を取る', () => {
     const vm = new VendingMachine();
     vm.send(10);
     expect(vm.sum).toBe(10);
   });
-})
+});
