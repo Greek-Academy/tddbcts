8a5ca7f6589d7c29a0695e5ae181c79a9bbec25d
masashi katsumata <wf9a5m75@gmail.com>
2023-09-09 00:37:16 -0700

    テストコードのリファクタリング

--- src/usagecases/__tests__/vendingMachine.spec.ts
+++ src/usagecases/__tests__/vendingMachine.spec.ts
@@ -1,37 +1,37 @@
 import {VendingMachine} from '../vendingMachine';
 describe('vendingMachine', () => {
+  let vm!: VendingMachine;
+  beforeEach(() => {
+    // テスト前にVendingMachineのインスタンスを作成する
+    vm = new VendingMachine(0);
+  })
+
   it('10円を投入して合計値を取る', () => {
-    const vm = new VendingMachine();
     vm.insert(10);
     expect(vm.balance).toBe(10);
   });
 
   it('50円を投入して合計値を取る', () => {
-    const vm = new VendingMachine();
     vm.insert(50);
     expect(vm.balance).toBe(50);
   });
 
   it('100円を投入して合計値を取る', () => {
-    const vm = new VendingMachine();
     vm.insert(100);
     expect(vm.balance).toBe(100);
   });
 
   it('500円を投入して合計値を取る', () => {
-    const vm = new VendingMachine();
     vm.insert(500);
     expect(vm.balance).toBe(500);
   });
 
   it('1000円札を投入して合計値を取る', () => {
-    const vm = new VendingMachine();
     vm.insert(1000);
     expect(vm.balance).toBe(1000);
   });
 
   it('投入は複数回できる', () => {
-    const vm = new VendingMachine();
     vm.insert(10);
     expect(vm.balance).toBe(10);
 
@@ -51,7 +51,6 @@ describe('vendingMachine', () => {
 
   it('払い戻し操作を行うと、投入金額の総計を釣り銭として出力する。', () => {
     
-    const vm = new VendingMachine();
     vm.insert(100);
     vm.insert(1000);
     vm.insert(1000);
--- src/usagecases/vendingMachine.ts
+++ src/usagecases/vendingMachine.ts
@@ -1,7 +1,9 @@
 type InsertType = 10 | 50 | 100 | 500 | 1000;
 
 export class VendingMachine {
-  private internalBalance: number = 0;
+  constructor(
+    private internalBalance: number = 0,
+  ) {}
 
   insert(num: InsertType) {
     this.internalBalance += num;
