30476c486d86f52a985faaa0a2b43f8822724168
阪本良介 <sakamoto@sumire-sakamoto.co.jp>
2023-09-09 16:09:19 +0900

    insertの実装

--- src/usagecases/__tests__/vendingMachine.spec.ts
+++ src/usagecases/__tests__/vendingMachine.spec.ts
@@ -5,4 +5,28 @@ describe('vendingMachine', () => {
     vm.insert(10);
     expect(vm.sum).toBe(10);
   });
+
+  it('50円を投入して合計値を取る', () => {
+    const vm = new VendingMachine();
+    vm.insert(50);
+    expect(vm.sum).toBe(50);
+  });
+
+  it('100円を投入して合計値を取る', () => {
+    const vm = new VendingMachine();
+    vm.insert(100);
+    expect(vm.sum).toBe(100);
+  });
+
+  it('500円を投入して合計値を取る', () => {
+    const vm = new VendingMachine();
+    vm.insert(500);
+    expect(vm.sum).toBe(500);
+  });
+
+  it('1000円札を投入して合計値を取る', () => {
+    const vm = new VendingMachine();
+    vm.insert(1000);
+    expect(vm.sum).toBe(1000);
+  });
 });
--- src/usagecases/vendingMachine.ts
+++ src/usagecases/vendingMachine.ts
@@ -1,7 +1,7 @@
 export class VendingMachine {
   public sum: number = 0;
 
-  insert(num: number) {
+  insert(num: 10 | 50 | 100 | 500 | 1000) {
     this.sum += num;
   }
 }
